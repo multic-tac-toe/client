5365124036445e721a8ca418998fd19443d7a406
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2020-01-23 20:52:13 +0700

    jap randomGEnerator randomJoinTeam randomInitalTeamTurn randomPlayerTUrn sendREsponse

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import db from '../config/firestore'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+<<<<<<< 5a7b95338d813b6949e2ab6fd1e3e71a32eafefc
     state: {
         roomID: ''
     },
@@ -28,4 +29,58 @@ export default new Vuex.Store({
         }
     },
     modules: {}
+=======
+  state: {
+      allPlayerList : ['aaa', 'bbb','ccc','ddd','eee'],
+      teamX : [],
+      teamO : [],
+      teamTurn : 'X',
+      playerTurn : '',
+      gameStat: {}
+
+
+  },
+  mutations: {
+      SET_TEAM_X(state, payload){
+          state.teamX = payload
+      },
+      SET_TEAM_O(state, payload){
+          state.teamO = payload
+      },
+      SET_TEAM_TURN(state, payload){
+          state.teamTurn = payload
+      },
+      SET_GAME_STAT(state, payload){
+          const key = Object.keys(payload)
+          state.gameStat[key] = payload[key]
+      }
+
+  },
+  actions: {
+      
+
+  },
+  modules: {
+  },
+  getters: {
+      allPlayerList: state =>{
+          return state.allPlayerList
+      },
+      teamX: state =>{
+          return state.teamX
+      },
+      teamO: state =>{
+          return state.teamO
+      },
+      teamTurn: state =>{
+          return state.teamTurn
+      },
+      playerTurn: state =>{
+          return state.playerTurn
+      },
+      gameStat: state=>{
+          return state.gameStat
+      }
+  }
+>>>>>>> jap
 })
