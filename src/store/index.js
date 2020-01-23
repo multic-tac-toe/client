d450525b3d3201bf3eef250f38c8046b435873ef
Immanuel Pandiangan <nuelpan@gmail.com>
2020-01-23 21:27:48 +0700

    add BoardComponent... add feature click by different team add feature computed vuex state to board add styling for board component

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,27 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    teamTurn: 'o',
+    playerTurn: 'jojo',
+    objGameState: {
+      1: 'x',
+      2: 'o',
+      3: '',
+      4: '',
+      5: '',
+      6: '',
+      7: '',
+      8: '',
+      9: ''
+    }
   },
   mutations: {
+    CHANGE_STATE(state, payload) {
+      state.objGameState[payload.location] = payload.team
+    }
   },
   actions: {
+    
   },
   modules: {
   }
---
+++ src/views/Arena.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="arena">
+    <b-container>
+      <b-row>
+        <b-col cols-lg="3">
+          <i class="fas fa-times big-icon"></i>
+          <hr />
+          <div class="players">
+            <div class="players-name">
+              somebody
+            </div>
+            <div class="players-name">
+              once
+            </div>
+            <div class="players-name">
+              told me
+            </div>
+          </div>
+        </b-col>
+        <b-col cols-lg="6" cols-sm="12">
+          <div class="BoardComponent-wrapper">
+            <BoardComponent />
+          </div>
+        </b-col>
+        <b-col cols-lg="3">
+          <i class="far fa-circle big-icon"></i>
+          <hr />
+          <div class="players">
+            <div class="players-name">
+              somebody
+            </div>
+            <div class="players-name">
+              once
+            </div>
+            <div class="players-name">
+              told me
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import BoardComponent from '@/components/BoardComponent.vue'
+
+export default {
+  name: 'arena',
+  components: {
+    BoardComponent
+  }
+}
+</script>
+<style scoped>
+  .big-icon {
+    font-size: 4rem;
+  }
+  .BoardComponent-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+</style>
