b8cb2362f5b6b7f728010a72b1e92526cab55efd
deprak <dedprakasa@gmail.com>
2020-01-23 19:10:56 +0700

    create room page

---
+++ src/components/CreateRoom.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container" style="width: 50vh">
+    <div>
+      <b-img
+        src="https://miro.medium.com/max/512/1*9LBDOx6gG4mTuPGPDp888w.png"
+        fluid
+        alt="Tic Tac Toe"
+        class="w-50 my-4 shadow"
+        rounded="circle"
+        style="transform: rotate(10deg)">
+      </b-img>
+    </div>
+    <b-form>
+      <b-form-group
+        id="input-group-1"
+        label="Create New Room"
+        label-for="new-room"
+        class="font-weight-bold"
+      >
+        <b-form-input
+          id="new-room"
+          type="text"
+          required
+          placeholder=""
+          class="rounded"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Create</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateRoom'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/RoomCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-card bg-variant="ligth" text-variant="dark" header="Room Name" class="room-card text-center">
+    <button type="button" class="btn btn-outline-dark">Join Room</button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'RoomCard'
+}
+</script>
+
+<style scoped>
+@keyframes zoominoutsinglefeatured {
+    0% {
+        transform: scale(1,1);
+    }
+    50% {
+        transform: scale(1.05,1.05);
+    }
+    100% {
+        transform: scale(1,1);
+    }
+}
+
+.room-card {
+	animation: zoominoutsinglefeatured 2s infinite !important;
+}
+</style>
