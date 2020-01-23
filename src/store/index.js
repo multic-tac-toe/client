b435e6b115bb7a49090848c697d64178f4c54f20
michaelwp <michael.wenceslaus@gmail.com>
2020-01-23 17:42:13 +0700

    init config for client and firestore

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import db from '../config/firestore'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
